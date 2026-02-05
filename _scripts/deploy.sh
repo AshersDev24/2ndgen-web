b4b627f661174a5ab04b1b7aa8aa6580f55c6a56
Ashley Hylton <bublhubmain@gmail.com>
2026-02-05 17:33:44 +0000

    fix(ops): align compose build contexts with repo layout

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -6,7 +6,7 @@ cd /srv/srv/2ndgen
 if [ -d .git ]; then
   git fetch origin main
   git reset --hard origin/main
-  git clean -fd
+  git clean -ffdx
 fi
 
 docker compose up -d --build api api_nginx queue web
@@ -14,4 +14,8 @@ docker compose up -d --build api api_nginx queue web
 docker exec 2ndgen_api sh -lc "php artisan config:clear && php artisan route:clear && php artisan view:clear && php artisan cache:clear"
 docker exec 2ndgen_api sh -lc "php artisan config:cache && php artisan route:cache && php artisan view:cache"
 
-./_scripts/health.sh
+if [ -x ./_scripts/health.sh ]; then
+  ./_scripts/health.sh
+else
+  docker compose ps
+fi
